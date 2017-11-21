a791771a537a7dd0f7c2fb40ac165d88dc1b79e4
Aviv Bueno <aviv.buen@gmail.com>
2017-11-22 01:08:04 +0200

    Fix

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var socket = require('socket.io');
-var connections = [];
+var fs = require("fs");
+
 // App setup
 var app  = express();
 var server = app.listen((process.env.PORT || 5000),function(){
     console.log("Server UP ON PORT: "+ (process.env.PORT || 5000));
+
 });
 
+//Get all active connections
+app.get('/test', function (req, res) {
+        // Get content from file
+    var contents = fs.readFileSync("todos.json");
+    // Define to JSON type
+    var jsonContent = JSON.parse(contents);
+    res.send(jsonContent);
+});
 // Static Routes
 app.use(express.static('public'));
 
@@ -16,6 +26,6 @@ var io = socket(server);
 io.on('connection',function(socket){
     //Handling an action of button click on the phone
     socket.on('change-notify', function(data){
-        socket.broadcast.to(connection.c_socketid).emit('acmedia',data);
+        socket.broadcast.emit('list-changed',data);
     });
 });
