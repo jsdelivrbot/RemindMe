f074ec4db721ad9a211e82eb7fda541428b1b465
Aviv Bueno <aviv.buen@gmail.com>
2017-12-15 20:16:26 +0200

    ds

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var socket = require('socket.io');
 var fs = require("fs");
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://dbUserName:dbpass@ds059207.mlab.com:59207/remindme";
 
 // App setup
 var app  = express();
 var server = app.listen((process.env.PORT || 5000),function(){
     console.log("Server UP ON PORT: "+ (process.env.PORT || 5000));
-
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        console.log("Database connected!");
+        db.close();
+    });
 });
 
 //Get all active connections
 app.get('/todos', function (req, res) {
-    let rawdata = fs.readFileSync('todos.json');  
-    let student = JSON.parse(rawdata);  
-    console.log(student);  
+
+
 });
 // Static Routes
 app.use(express.static('public'));
@@ -27,12 +32,23 @@ io.on('connection',function(socket){
         data = data.replace("/", "");
         console.log("pbj"+ JSON.stringify(data))
         // Get content from file
-        let jsonData = require('./todos.json');
-        
+        //let jsonData = require('./todos.json');
+       /* 
         jsonData.todos.push(JSON.parse(data));
         fs.writeFile("todos.json", jsonData, function (err) {
             if (err) return console.log(err);
         });
-        socket.broadcast.emit('list-changed',JSON.stringify(jsonData));
+        */
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var myobj = data;
+            db.collection("todos").insertOne(myobj, function(err, res) {
+              if (err) throw err;
+              console.log("1 document inserted");
+              
+              db.close();
+            });
+          });
+        
     });
 });
